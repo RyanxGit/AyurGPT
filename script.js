a93d33dd5834ef9b8c3cf0ac89d9afbcf1be948c
RyanxGit <21eg105f18@anurag.edu.in>
2023-09-18 17:20:43 +0530

    modified

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const body = document.querySelector("body"),
  toggle = body.querySelector(".toggle"),
  searchbtn = body.querySelector(".search-box"),
  modeswitch = body.querySelector(".toggle-switch"),
- modetext = body.querySelector(".mode-text");
+ modetext = body.querySelector(".mode-text"),
+ servicelink = document.getElementById("services-link"),
+ ayurbotlink=document.getElementById("ayurbot-link"),
+
+ servicecontent=document.getElementById("service-cont"),
+ ayurcontent=document.getElementById("ayurbot-content");
 
  toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
@@ -19,3 +24,24 @@ const body = document.querySelector("body"),
     }
     
  });
+function playVideo(b){
+  const video = document.getElementById(b);
+  video.play();
+  
+}
+ function pauseVideo(b){
+  const video = document.getElementById(b);
+  video.pause()
+ }
+//  function showserviecs(){
+//   servicecontent.style.display="block";
+//   ayurcontent.style.display="none";
+//  }
+//  function showayurbot(){
+//   servicecontent.style.display="none";
+//   ayurcontent.style.display="block";
+//  }
+//  servicelink.addEventListener("click",showserviecs);
+//  ayurbotlink.addEventListener("click",showayurbot);
+
+//    showayurbot();
